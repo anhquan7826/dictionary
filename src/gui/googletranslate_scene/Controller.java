c481c43cd96b074bd7d0251041e66b5ff790325c
anhquan7826 <anhquan7826@gmail.com>
2021-10-12 16:58:29 +0700

    remove unused imports

--- src/gui/googletranslate_scene/Controller.java
+++ src/gui/googletranslate_scene/Controller.java
@@ -5,16 +5,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ResourceBundle;
+
+import dictionary.Operate;
 
 public class Controller implements Initializable {
 
@@ -30,39 +27,17 @@ public class Controller implements Initializable {
     @FXML
     WebView explainEV = new WebView();
 
-    private static String translate (String langFrom, String langTo, String text) throws IOException {
-
-        // URL API google translate.
-        String urlStr = "https://script.google.com/macros/s/AKfycbz42DCnvNQ8hOxRkKcwf4Vq7_eoB5W7xRnIb3e0b3z7bJ0ukZxMq2Oa6_6dujen9motaw/exec" +
-                "?q=" + URLEncoder.encode(text, "UTF-8") +
-                "&target=" + langTo +
-                "&source=" + langFrom;
-        URL url = new URL(urlStr);
-        StringBuilder response = new StringBuilder();
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        return response.toString();
-    }
-
     public void translateVE(KeyEvent event) throws IOException {
         if (event.getCode().equals(KeyCode.ENTER)){
             String meaning = targetVE.getText();
-            WebEngine engine = explainVE.getEngine();
-            explainVE.getEngine().loadContent(translate("vi","en", meaning) , "text/html");
+            explainVE.getEngine().loadContent(Operate.Translate.translate("vi","en", meaning) , "text/html");
         }
     }
 
     public void translateEV(KeyEvent event) throws IOException {
         if (event.getCode().equals(KeyCode.ENTER)){
             String meaning = targetEV.getText();
-            WebEngine engine = explainEV.getEngine();
-            explainEV.getEngine().loadContent(translate("en","vi", meaning) , "text/html");
+            explainEV.getEngine().loadContent(Operate.Translate.translate("en","vi", meaning) , "text/html");
         }
     }
 
