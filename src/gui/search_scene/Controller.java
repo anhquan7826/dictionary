8c04a9cd2e457d047554337c479dfb526e13c6a5
anhquan7826 <anhquan7826@gmail.com>
2021-10-14 15:30:14 +0700

    added history and favorite

--- src/gui/search_scene/Controller.java
+++ src/gui/search_scene/Controller.java
@@ -1,9 +1,12 @@
 package gui.search_scene;
 
 import dictionary.Operate;
+import dictionary.Operate.Favorite;
 import dictionary.manager.word.Word;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
@@ -11,18 +14,39 @@ import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML
-    private TextField search = new TextField();
-
-    @FXML
-    private ListView<String> searchResult = new ListView<>();
-
+    // main stuff
     @FXML
     private WebView viewWord = new WebView();
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        searchResult.getItems().setAll(Operate.EVDictionary.getData().keySet());
+    }
+
+    private Word wordBeingDisplayed;
+
+    private void showSelectedWord(String selectedItem) {
+        if (selectedItem == null) {return;}
+        if (dictChoice.getText().equals("Eng - Vi")) {
+            wordBeingDisplayed = Operate.EVDictionary.getWord(selectedItem);
+            if (Favorite.getData().containsKey(selectedItem)) {
+                favCheckBox.setSelected(true);
+            } else {
+                favCheckBox.setSelected(false);
+            }
+        } else {
+            wordBeingDisplayed = Operate.VEDictionary.getWord(selectedItem);
+        }
+        viewWord.getEngine().loadContent(wordBeingDisplayed.getWord_explain(), "text/html");
+        historyStack.push(wordBeingDisplayed);
+    }
+
+    // mode choosing
     @FXML
     private MenuButton dictChoice = new MenuButton();
         @FXML
@@ -30,43 +54,30 @@ public class Controller implements Initializable {
         @FXML
         private MenuItem veChoice = new MenuItem();
 
-/*     @FXML
-    private ChoiceBox<String> dictChoiceBox = new ChoiceBox<String>();
-    ObservableList<String> dictChoice = FXCollections.observableArrayList("Eng - Vi", "Vi - Eng"); */
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        searchResult.getItems().setAll(Operate.EVDictionary.getData().keySet());
-
-/*         dictChoiceBox.setItems(dictChoice);
-        dictChoiceBox.setValue(dictChoice.get(0)); */
-    }
-
-
-/*     public void listAllWordSwitcher() {
-        if (dictChoice.getText().equals("Eng - Vi")) {
-            search.clear();
-            searchResult.getItems().setAll(Operate.EVDictionary.getData().keySet());
-        } else {
-            search.clear();
-            searchResult.getItems().setAll(Operate.VEDictionary.getData().keySet());
-        }
-    } */
     @FXML
     public void chooseEV() {
         dictChoice.setText("Eng - Vi");
+        favCheckBox.setDisable(false);
         search.clear();
         searchResult.getItems().setAll(Operate.EVDictionary.getData().keySet());
     }
     @FXML
     public void chooseVE() {
         dictChoice.setText("Vi - Eng");
+        favCheckBox.setDisable(true);
         search.clear();
         searchResult.getItems().setAll(Operate.VEDictionary.getData().keySet());
     }
 
+    // search field
     @FXML
-    public void resultOnType() {
+    private TextField search = new TextField();
+
+    @FXML
+    private ListView<String> searchResult = new ListView<>();
+
+    @FXML
+    public void showResultList() {
         if (dictChoice.getText().equals("Eng - Vi")) {
             if (search.getText().isBlank()) {
                 searchResult.getItems().setAll(Operate.EVDictionary.getData().keySet());
@@ -83,14 +94,82 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    public void showSelectedWord() {
-        String key = searchResult.getSelectionModel().getSelectedItem();
-        Word word;
-        if (dictChoice.getText().equals("Eng - Vi")) {
-            word = Operate.EVDictionary.getWord(key);
-        } else {
-            word = Operate.VEDictionary.getWord(key);
+    public void showResultItemOnSelect() {
+        showSelectedWord(searchResult.getSelectionModel().getSelectedItem());
+    }
+
+    // favorite field
+    @FXML
+    private CheckBox favCheckBox = new CheckBox();
+
+    @FXML
+    private ListView<String> favList = new ListView<>();
+
+    @FXML
+    public void showFavoriteList() {
+        favList.getItems().setAll(Operate.Favorite.getData().keySet());
+    }
+
+    @FXML
+    public void favCheckBoxOnAction() {
+        if (!favCheckBox.isDisabled()) {
+            if (favCheckBox.isSelected()) {
+                Favorite.addWord(wordBeingDisplayed);
+                showFavoriteList();
+            } else {
+                Favorite.removeWord(wordBeingDisplayed.getWord_target());
+                showFavoriteList();
+            }
         }
-        viewWord.getEngine().loadContent(word.getWord_explain(), "text/html");
+    }
+
+    @FXML
+    public void showFavoriteItemOnSelect() {
+        showSelectedWord(favList.getSelectionModel().getSelectedItem());
+    }
+
+    // history field
+    @FXML
+    private ListView<String> historyList = new ListView<>();
+
+    @FXML
+    private Button historyClearButton = new Button();
+
+    private class Stack {
+        private List<Word> stack;
+
+        private Stack() {
+            stack = new ArrayList<Word>();
+        }
+    
+        private void push(Word value) {
+            stack.add(0, value);
+        }
+
+        private void clear() {
+            stack.clear();
+        }
+
+        private List<String> toStringList() {
+            List<String> r = new ArrayList<>();
+            for (Word i : stack) {
+                r.add(i.getWord_target());
+            }
+            return r;
+        }
+    }
+
+    private Stack historyStack = new Stack();
+
+    @FXML
+    public void showHistoryList() {
+        if (historyStack == null) {return;}
+        historyList.getItems().setAll(historyStack.toStringList());
+    }
+
+    @FXML
+    public void clearHistory() {
+        historyStack.clear();
+        historyList.getItems().clear();
     }
 }
