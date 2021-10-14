48bec448495bf557294ee099ec5d4986cf015878
anhquan7826 <anhquan7826@gmail.com>
2021-10-14 15:55:18 +0700

    update database on close

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import dictionary.Operate;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -17,5 +19,17 @@ public class Main extends Application {
         primaryStage.setTitle("Test");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent arg0) {
+                try {
+                    Operate.EVDictionary.updateData();
+                    Operate.VEDictionary.updateData();
+                    Operate.Favorite.updateData();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
