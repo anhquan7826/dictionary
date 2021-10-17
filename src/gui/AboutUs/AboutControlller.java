0307e1c8b73201fae31674f6664d1fb758fe2c4a
Lppt12345 <aibiettoi19@gmail.com>
2021-10-17 10:06:38 +0700

    Fix URL

--- src/gui/AboutUs/AboutControlller.java
+++ src/gui/AboutUs/AboutControlller.java
@@ -2,32 +2,46 @@ package gui.AboutUs;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import java.awt.Desktop;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AboutControlller implements Initializable {
     // MAC use Runtime.getRuntime().exec(new String[]{"/usr/bin/open", "-a", "/Applications/Google Chrome.app", "write URL here"});
     @FXML
-    public void OpenLinkQuan () throws IOException {
+    public void OpenLinkQuan () throws IOException{
         // Window
-        Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://github.com/anhquan7826"});
+        //Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://github.com/anhquan7826"});
         // Linux
         // Runtime.getRuntime().exec(new String[]{"bash", "-c", "/path/to/chrome https://github.com/anhquan7826"});
+        //ALl
+        Desktop desktop = Desktop.getDesktop();
+        String url_open ="https://github.com/anhquan7826";
+        Desktop.getDesktop().browse(URI.create(url_open));
     }
     @FXML
     public void OpenLinkHa () throws IOException {
         // Window
-        Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://github.com/Lppt12345"});
+        //Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://github.com/Lppt12345"});
         // Linux
         // Runtime.getRuntime().exec(new String[]{"bash", "-c", "/path/to/chrome https://github.com/Lppt12345"});
+        // ALL
+        Desktop desktop = Desktop.getDesktop();
+        String url_open ="https://github.com/Lppt12345";
+        Desktop.getDesktop().browse(URI.create(url_open));
     }
     @FXML
     public void OpenProject() throws IOException {
         // Window
-        Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://github.com/anhquan7826/dictionary"});
+        //Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://github.com/anhquan7826/dictionary"});
         // Linux
         // Runtime.getRuntime().exec(new String[]{"bash", "-c", "/path/to/chrome https://github.com/anhquan7826/dictionar"});
+        // ALL
+        Desktop desktop = Desktop.getDesktop();
+        String url_open ="https://github.com/anhquan7826/dictionary";
+        Desktop.getDesktop().browse(URI.create(url_open));
     }
 
     @Override
