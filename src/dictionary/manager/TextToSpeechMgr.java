22ab23f5b447615076246d47ffe3da7a97d67f77
anhquan7826 <anhquan7826@gmail.com>
2021-10-25 15:32:08 +0700

    tts fix

--- src/dictionary/manager/TextToSpeechMgr.java
+++ src/dictionary/manager/TextToSpeechMgr.java
@@ -13,18 +13,14 @@ public class TextToSpeechMgr {
             // Set property as Kevin Dictionary
             System.setProperty(
                     "freetts.voices",
-                    "com.sun.speech.freetts.en.us"
-                            + ".cmu_us_kal.KevinVoiceDirectory");
+                    "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
 
             // Register Engine
             Central.registerEngineCentral(
-                    "com.sun.speech.freetts"
-                            + ".jsapi.FreeTTSEngineCentral");
+                    "com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");
 
             // Create a Synthesizer
-            Synthesizer synthesizer
-                    = Central.createSynthesizer(
-                    new SynthesizerModeDesc(Locale.US));
+            Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
 
             // Allocate synthesizer
             synthesizer.allocate();
@@ -34,10 +30,8 @@ public class TextToSpeechMgr {
 
             // Speaks the given text
             // until the queue is empty.
-            synthesizer.speakPlainText(
-                    text, null);
-            synthesizer.waitEngineState(
-                    Synthesizer.QUEUE_EMPTY);
+            synthesizer.speakPlainText(text, null);
+            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
 
             // Deallocate the Synthesizer. (Need Set Stop condition)
             // synthesizer.deallocate();
